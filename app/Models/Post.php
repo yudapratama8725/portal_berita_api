235346b3adfc94af695b7c01abffb8434b494015
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-03-19 14:30:17 +0700

    Api Resource Collection

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'news_content', 'author'
+    ];
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function writer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author', 'id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/posts');
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts2/{id}', [PostController::class, 'show2']);
+
+// Route::get('/posts', function () {
+//     dd('Coba Api');
+// });
