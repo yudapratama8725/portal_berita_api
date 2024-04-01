7f49ab6a27e7e249d83b3f6f840af32089fbd922
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-04-01 15:44:28 +0700

    Edit Route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::get('/posts2/{id}', [PostController::class, 'show2']);
 
